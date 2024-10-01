62f2985c02cb67d05f016c3c737e4090966b8404
oliver-ricken <2oliver.ricken@gmail.com>
2024-10-01 13:02:01 -0700

    added split into chunks function

--- docsum.py
+++ docsum.py
@@ -1,3 +1,21 @@
+def split_document_into_chunks(text, chunk_size=5000):
+	"""
+	Split text into smaller chunks so an LLM can process those chunks individually.
+
+	Args:
+	  text (str): The input text to split into chunks
+	  chunk_size (int): The maximum size of each chunk in terms of characters.
+
+	Returns:
+	  List[str]: A list of text chunks.
+	"""
+	# we split the text into chunks by the given chunk size
+	chunks = [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
+	return chunks
+	
+
+
+
 from dotenv import load_dotenv
 import os
 from groq import Groq
@@ -14,7 +32,7 @@ chat_completion = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "Explain the importance of low latency LLMs",
+            "content": "Explain the importance of low latency LLMs in one sentence only.",
         }
     ],
     model="llama3-8b-8192",
